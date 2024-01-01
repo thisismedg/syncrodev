7a69789ecdba9c06c37794710a91ec8a5b6db678
itsme-dg <itsmeeeee.dg@gmail.com>
2024-01-01 11:14:13 +0800

    Added Clerk Auth

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import '@/styles/globals.scss'
+import type { Metadata } from "next";
+import "@/styles/globals.scss";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: 'Syncro.dev',
-  description: 'Syncro.dev',
-}
+  title: "Syncro.dev",
+  description: "Syncro.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
